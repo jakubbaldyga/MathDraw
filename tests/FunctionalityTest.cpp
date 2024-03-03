7ca1bff9ffb3ff025705465f58fb904eccc3a107
jakubbaldyga <kubszcz@gmail.com>
2024-03-03 11:16:25 +0100

    updated CmakeList.txt

--- tests/FunctionalityTest.cpp
+++ tests/FunctionalityTest.cpp
@@ -1,6 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include "../src/Utilities/Strings.hpp"
-#include "../src/Utilities//Utilties.hpp"
+#include "../src/Utilities/Utilties.hpp"
 #include <SFML/System/Vector2.hpp>
 #include "../src/AI/PixelTraversal/PixelTraversal.hpp"
 #include "../src/AI/ScaleImage/ScaleImage.hpp"
@@ -27,25 +27,25 @@ bool checkThrows(Function&& func) {
 
 TEST_CASE("AI check") {
 	sf::Image image;
-	image.loadFromFile("../../../res/test/empty.png");
-	AI ai("../../../res/model.pt");
+	image.loadFromFile("res/test/empty.png");
+	AI ai("model.pt");
 
 	auto func = std::bind(&AI::evaluate, ai, image, 10);
 	REQUIRE(checkThrows(func) == true);
 
-	image.loadFromFile("../../../res/test/one.png");
+	image.loadFromFile("res/test/one.png");
 	REQUIRE(ai.evaluate(image, 10).second == 1);
 
-	image.loadFromFile("../../../res/test/two.png");
+	image.loadFromFile("res/test/two.png");
 	REQUIRE(ai.evaluate(image, 10).second == 2);
 
-	image.loadFromFile("../../../res/test/three.png");
+	image.loadFromFile("res/test/three.png");
 	REQUIRE(ai.evaluate(image, 10).second == 3);
 
-	image.loadFromFile("../../../res/test/four.png");
+	image.loadFromFile("res/test/four.png");
 	REQUIRE(ai.evaluate(image, 10).second == 4);
 
-	image.loadFromFile("../../../res/test/12.png");
+	image.loadFromFile("res/test/12.png");
 	REQUIRE(ai.evaluate(image, 10).first == "12");
 	REQUIRE(ai.evaluate(image, 10).second == 12);
 	
@@ -55,22 +55,22 @@ TEST_CASE("AI check") {
 
 TEST_CASE("Algorithms check") {
 	sf::Image image;
-	image.loadFromFile("../../../res/test/empty.png");
+	image.loadFromFile("res/test/empty.png");
 
 	REQUIRE(PixelTraversal::getImages(image).size() == 0);
 
-	image.loadFromFile("../../../res/test/12.png");
+	if(!image.loadFromFile("res/test/12.png")) REQUIRE(false);
 
 	auto result = PixelTraversal::getImages(image);
 	sf::Image im1, im2;
-	im1.loadFromFile("../../../res/test/traversedImage0.png");
-	im2.loadFromFile("../../../res/test/traversedImage1.png");
+	im1.loadFromFile("res/test/traversedImage0.png");
+	im2.loadFromFile("res/test/traversedImage1.png");
 
 	REQUIRE(result.size() == 2);
 	REQUIRE(result[0] == im1);
 	REQUIRE(result[1] == im2);
 
 	sf::Image scaledImage = ScaleImage::scaleImage(image, sf::Vector2i(10, 10));
-	im1.loadFromFile("../../../res/test/scaledImage.png");
+	im1.loadFromFile("res/test/scaledImage.png");
 	REQUIRE(scaledImage == im1);
 }
